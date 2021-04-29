f0f9b98310f43e805d67ae2154a63695c0544811
SKKUGoon <baesangil0906@gmail.com>
2021-04-29 19:18:35 +0900

    [CodeFix] _ will be deprecated

--- workers/THREAD_trader.py
+++ workers/THREAD_trader.py
@@ -1,9 +1,7 @@
-from models.model_config import *
 from main.KWDERIV_order_spec import *
-from _code.trade_state import individual as individual_trade
-from _util.UTIL_log import *
-from _util.set_order import OrderSheet
-from models.oms_v1 import *
+from code_.trade_state import individual as individual_trade
+from util.UTIL_log import *
+from util.set_order import OrderSheet
 from PyQt5.QtCore import *
 
 import copy
@@ -21,23 +19,9 @@ class Trader(QThread):
     req_compare = pyqtSignal(tuple)
     log = pyqtSignal(tuple)
 
-    def __init__(self, model:OMS, time_dict:Dict, trade_value, screen_num, order_spec:OrderSpec,
+    def __init__(self, model, time_dict:Dict, trade_value, screen_num, order_spec:OrderSpec,
                  target_value=None, loss_cut=None, co_pair=False, timeout=None):
         super().__init__()
-        # Model related Thread parameters
-        self.model = model
-        self.scr = screen_num
-        self.name = self.model.strategy
-        self.time_table = self.set_time_format(time_dict)
-        print(f"{self.name}'s token number is {self.model.token['token_number']}")
-
-        # Trade related Thread parameters
-        self.loss = loss_cut
-        self.maximum = trade_value
-        self.halt = target_value
-        self.timeout = timeout
-        self.sheet = OrderSheet()
-        self.spec = order_spec
 
         # Compare related Thread parameters
         self.compare = co_pair
@@ -134,81 +118,4 @@ class Trader(QThread):
         self.create_model_status(self.local, self.name)
 
         while self.isRunning():
-            status = self.local.select_db(
-                target_column=['trade_status'],
-                target_table='trade_state',
-                condition1=f"model_name='{self.name}'"
-            )[0][0]
-            try:
-                t = self.local.select_db(
-                    target_column=['server_time'], target_table='RT_Option')[0][0]
-                if int(t) == 0:
-                    raise Exception
-            except Exception:
-                t = datetime.datetime.now().strftime('%H%M%S')
-
-            if self.chk_condition(status, 'get_asset_buy', self.state, t, 'get_asset', self.time_table):
-                if self.singleshot[status] is False:
-                    ex = copy.deepcopy(self.singleshot)
-                    self.get_asset.emit(
-                        (self.model, self.name, self.loc, ex, status+1)
-                    )
-                    self.set_state(self.local, status+1, self.name)
-                    self.singleshot[status] = True
-                else:
-                    self.log.emit((f'{self.name} get_asset_buy already done', 'debug'))
-
-            if self.chk_condition(status, 'get_rt_prc', self.state):
-                if self.singleshot[status] is False:
-                    ex = copy.deepcopy(self.singleshot)
-                    self.get_price.emit(
-                        (self.scr, self.name, self.loc, ex, status+1)
-                    )
-                    self.singleshot[status] = True
-                else:
-                    self.log.emit((f'{self.name} get_rt_prc already done', 'debug'))
-
-            if self.chk_condition(status, 'send_main_thread', self.state):
-                if self.compare is True:
-                    if self.singleshot[status] is False:
-                        self.req_compare.emit((self.name, self.token, status+1))
-                        self.signleshot[status] = True
-                else:
-                    self.set_state(self.local, status+1, self.name)
-
-            if self.chk_condition(status, 'buy_asset', self.state, t, 'trade_start', self.time_table):
-                # This Thread
-                if self.singleshot[status] is False:
-                    ...
-                    self.singleshot[status] = True
-
-            if self.chk_condition(status, 'buy_complete', self.state):
-                # Go to Main Thread
-                if self.singleshot[status] is False:
-                    ...
-                    self.singleshot[status] = True
-
-            if self.chk_condition(status, 'sell_asset', self.state, t, 'trade_end', self.time_table):
-                # This Thread
-                if self.singleshot[status] is False:
-                    ...
-                    self.singleshot[status] = True
-
-            if self.chk_condition(status, 'sell_complete', self.state):
-                # Go to Main Thread
-                if self.singleshot[status] is False:
-                    ...
-                    self.singleshot[status] = True
-
-
-            self.log.emit((f'{self.name} running on {threading.currentThread().getName()}. Model state: {status}', 'debug'))
-            self.sleep(1)
-
-
-
-        # self.local_event_loop = QEventLoop()
-        # self.timer = QTimer(self)
-        # self.timer.singleShot(3000, self.login_callback)
-        # self.local_event_loop.exec()            # 이 라인에서 self.local_event_loop가 quit될 때까지 대기 (이벤트는 처리됨)
-        # self.check_balance()
-
+            ...
