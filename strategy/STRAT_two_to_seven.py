6265ed670ff11a914eae0d39d74eb0eac7ecf2e8
SKKUGoon <baesangil0906@gmail.com>
2021-04-30 18:06:47 +0900

    [CodeFix] Trade Parameter

--- strategy/STRAT_two_to_seven.py
+++ strategy/STRAT_two_to_seven.py
@@ -1,26 +1,24 @@
 from strategy.__init__ import FTManager
+from util.UTIL_asset_code import get_exception_date
 
+import datetime
 
 class FTTwoSeven(FTManager):
+
     def get_state(self):
-        states = {
-            'get_asset' : {
-                'state' : 0, 'time' : '090010'
-            },
-            'get_price' : {
-                'state' : 1, 'time' : 'any'
-            },
-            'get_prediction' : {
-                'state' : 2, 'time' : '090200'
-            },
-            'buy' : {
-                'state' : 3, 'time' : '090200'
-            },
-            'sell' : {
-                'state' : 4, 'time' : '090700'
-            }
-        }
-        return states
+        sat = get_exception_date('SAT')
+        fb = get_exception_date('1stBusinessDay')
+
+        today = datetime.datetime.now().strftime('%Y%m%d')
+        if (today in sat) or (today in fb):
+            states = ['100100', '100200']
+            limit = ['100120', '100120']
+
+        else:
+            states = ['090100', '090200']
+            limit = ['090120', '090120']
+
+        return states, limit
 
     def my_name(self):
         return 'FTTwoSeven'
