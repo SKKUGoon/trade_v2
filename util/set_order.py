c3081076c6e1c292e077da3eba494be1a2674414
SKKUGoon <baesangil0906@gmail.com>
2021-04-30 18:05:17 +0900

    [CodeFix] order sheet maker

--- util/set_order.py
+++ util/set_order.py
@@ -1,3 +1,20 @@
+def order_base(name, scr_num, account, asset, buy_sell, trade_type, quantity, price, order_num=""):
+    order = {
+        'rq_name': name,
+        'screen_num': scr_num,
+        'account': account,
+        'code': asset,
+        'order_type': 1,
+        'buy_sell': buy_sell,
+        'trade_type': trade_type,
+        'quantity': quantity,
+        'price': price,
+        'order_num': order_num
+    }
+    return order
+
+
+
 class OrderSheet:
     def __init__(self):
         pass
