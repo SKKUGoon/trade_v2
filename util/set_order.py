4c4ff13fb19e1c6eae796a367e6ca090106c7551
SKKUGoon <baesangil0906@gmail.com>
2021-05-04 18:03:35 +0900

    [CodeFix] Add order type(cancel(3), new submit(1), refract(2)).

--- util/set_order.py
+++ util/set_order.py
@@ -1,10 +1,11 @@
-def order_base(name, scr_num, account, asset, buy_sell, trade_type, quantity, price, order_num=""):
+def order_base(name, scr_num, account, asset, buy_sell, trade_type, quantity, price,
+               order_type=1, order_num=""):
     order = {
         'rq_name': name,
         'screen_num': scr_num,
         'account': account,
         'code': asset,
-        'order_type': 1,
+        'order_type': order_type,
         'buy_sell': buy_sell,
         'trade_type': trade_type,
         'quantity': quantity,
